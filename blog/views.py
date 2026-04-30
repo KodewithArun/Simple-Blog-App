d8bcadd63463d5b6edf2d4a27bc5958e38ec9224
KodewithArun <contactwitharunlaudari@gmail.com>
2026-04-30 13:25:36 +0545

    feat: implement Django backend flow (model, view, template, url)

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render 
 from django.http import HttpResponse
+from .models import Post
 
 def home(request):
-    return HttpResponse("Welcome to Home Page Arun")
+    posts = Post.objects.all()
+    return render(request,'home.html',{'posts':posts})
+
