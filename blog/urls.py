e2cd9a3a89b82d4408fa38a65301e53ee6a5c70d
KodewithArun <contactwitharunlaudari@gmail.com>
2026-04-30 15:46:51 +0545

    feat(auth): add user registration , login , logout and protected pages

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import home, create_post , post_details ,delete_post , edit_post
+from .views import home, create_post , post_details ,delete_post , edit_post , signup , login_view , logout_view
 
 urlpatterns = [
+    path("signup/",signup, name="signup"),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
     path('',home,name='home'),
     path('create/',create_post,name='create'),
     path('post/<int:id>/',post_details,name='detail'),
--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,54 @@
+from django.contrib.auth.models import User
+from django.contrib.auth import login , authenticate , logout
+from django.contrib.auth.decorators import login_required
+
 from django.shortcuts import render , redirect , get_object_or_404
 from django.http import HttpResponse
 from .models import Post
 from .forms import PostForm
 
+def signup(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = User.objects.create_user(
+            username=username,
+            password=password
+        )
+
+        login(request, user)
+        return redirect('home')
+
+    return render(request, 'signup.html')
+
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+
+    return render(request, 'login.html')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
+
 def home(request):
+    if not request.user.is_authenticated:
+        return render(request, 'welcome.html')
     posts = Post.objects.all()
-    return render(request,'home.html',{'posts':posts})
+    return render(request, 'home.html', {'posts': posts})
 
 
-
+@login_required
 def create_post(request):
     if request.method == "POST":
         title = request.POST.get("title")
@@ -28,12 +68,15 @@ def post_details(request,id):
     post = get_object_or_404(Post,id=id)
     return render(request, 'post_detail.html',{'post':post})
 
+@login_required
+@login_required
 def delete_post(request,id):
     post = get_object_or_404(Post,id=id)
     post.delete()
     return redirect('home')
 
 
+@login_required
 def edit_post(request, id):
     post = get_object_or_404(Post, id=id)
 
